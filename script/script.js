b3253dff416e59a2c0ffb8691b5dae008b23a458
4004h <minjihwan9500@gmail.com>
2024-07-09 11:28:21 +0900

    header v1

---
+++ script/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $("ul.head-main-menu>li:not(:first-child)").mouseover(function () {
+        // console.log($(this).children().last())
+        $(this).children().last().stop().fadeIn(200)
+    })
+    $("ul.head-main-menu>li:not(:first-child)").mouseleave(function () {
+        // console.log($(this).children().last())
+        $(this).children().last().stop().fadeOut(200)
+    })
+
+    $("ul.head-sub-menu>li:has(ul.head-sub-menu-sub)").mouseover(function () {
+        console.log($(this).children().last())
+        $(this).children().last().stop().fadeIn(200)
+    })
+    $("ul.head-sub-menu>li:has(ul.head-sub-menu-sub)").mouseleave(function () {
+        console.log($(this).children().last())
+        $(this).children().last().stop().fadeOut(200)
+    })
+})
