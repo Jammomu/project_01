5a9f4d53a2d6c7bc7f9e0f8e7ed8b19f0ac7a348
KDH-YS <gloian@naver.com>
2024-07-24 17:18:20 +0900

    7.24

--- react_project/src/project02/header.jsx
+++ react_project/src/project02/header.jsx
@@ -1,7 +1,24 @@
-import './style/header.css';
+import { useState } from 'react';
 import logoWhite from './img/logo_white.png';
 import { MyComponent } from './img/Arrowdown';
 export function Header() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [subHovered, setSubHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);}
+
+    const subhandleMouseEnter = () => {
+      setSubHovered(true);
+    };
+  
+    const subhandleMouseLeave = () => {
+      setSubHovered(false);}
+
   return (
     <>
       <header>
@@ -28,9 +45,9 @@ export function Header() {
                             </li>
                             <li>
                                 <a href="./sub-page1-1-1.html"> 회사소개 <MyComponent/> </a>
-                                <ul class="head-sub-menu">
+                                <ul class='head-sub-menu'>
                                     <li><a href="./sub-page1-1-1.html"> 회사소개 </a>
-                                        <ul class="head-sub-menu-sub">
+                                        <ul class='head-sub-menu-sub'>
                                             <li><a href="./sub-page1-1-1.html"> 회사소개 스타일1 </a></li>
                                             <li><a href="./sub-page1-1-2.html"> 회사소개 스타일2 </a></li>
                                             <li><a href="./sub-page1-1-3.html"> 회사소개 스타일3 </a></li>
